3f1a6435186a41932d4c25f0172fb8f4dc6ed57a
Ivano <ivanomonego@yahoo.it>
2024-03-25 22:39:44 +0100

    Added a function to play an entire game

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-console.log("Hello, World!");
-
-let computerSelection;
-let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-  computerSelection = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+  let computerSelection = Math.floor(Math.random() * (3 - 1 + 1) + 1);
 
   switch (computerSelection) {
     case 1:
@@ -21,34 +19,71 @@ function getComputerChoice() {
   return computerSelection;
 }
 
+function getPlayerChoice() {
+  return prompt("Player choice is...").toLowerCase();
+}
+
 function rPSRound(playerSelection, computerSelection) {
+  console.log(
+    `You chose ${playerSelection}, while computer chose ${computerSelection}.`
+  );
   if (playerSelection === "rock") {
     if (computerSelection === "rock") {
       console.log("Rock and rock. That's a tie!");
     } else if (computerSelection === "scissors") {
       console.log("Rock beats scissors. You win!");
+      playerScore += 1;
     } else {
       console.log("Paper beats rock. You lose!");
+      computerScore += 1;
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "paper") {
       console.log("Paper and paper. That's a tie!");
     } else if (computerSelection === "rock") {
       console.log("Paper beats rock. You win!");
+      playerScore += 1;
     } else {
       console.log("Scissors beat paper. You lose!");
+      computerScore += 1;
     }
   } else {
     if (computerSelection === "scissors") {
       console.log("Scissors and scissors. That's a tie!");
     } else if (computerSelection === "paper") {
       console.log("Scissors beats paper. You win!");
+      playerScore += 1;
     } else {
-      console.log("Rcok beats scissors. You lose!");
+      console.log("Rock beats scissors. You lose!");
+      computerScore += 1;
     }
   }
 }
 
 function playGame() {
-  rPSRound();
+  rPSRound(getPlayerChoice(), getComputerChoice());
+  showScore();
+  rPSRound(getPlayerChoice(), getComputerChoice());
+  showScore();
+  rPSRound(getPlayerChoice(), getComputerChoice());
+  showScore();
+  rPSRound(getPlayerChoice(), getComputerChoice());
+  showScore();
+  rPSRound(getPlayerChoice(), getComputerChoice());
+  showScore();
+}
+
+function showScore() {
+  console.log(`Player score is: ${playerScore}.
+Computer score is: ${computerScore}.`);
+}
+
+playGame();
+
+if (playerScore > computerScore) {
+  console.log("Player wins.");
+} else if (playerScore < computerScore) {
+  console.log("Computer wins.");
+} else {
+  console.log("It's a tie.");
 }
