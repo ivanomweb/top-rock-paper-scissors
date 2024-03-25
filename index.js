9f1c97a4837b4851b6e8d5df8a97be8f1d87e772
Ivano <ivanomonego@yahoo.it>
2024-03-25 22:14:10 +0100

    Added a function to play a single round

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
 console.log("Hello, World!");
 
-let computerChoice;
+let computerSelection;
+let playerSelection;
 
 function getComputerChoice() {
-  computerChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+  computerSelection = Math.floor(Math.random() * (3 - 1 + 1) + 1);
 
-  switch (computerChoice) {
+  switch (computerSelection) {
     case 1:
-      computerChoice = "Rock";
+      computerSelection = "rock";
       break;
     case 2:
-      computerChoice = "Paper";
+      computerSelection = "paper";
       break;
     case 3:
-      computerChoice = "Scissors";
+      computerSelection = "scissors";
       break;
   }
 
-  return computerChoice;
+  return computerSelection;
 }
 
-console.log(getComputerChoice());
+function rPSRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock") {
+    if (computerSelection === "rock") {
+      console.log("Rock and rock. That's a tie!");
+    } else if (computerSelection === "scissors") {
+      console.log("Rock beats scissors. You win!");
+    } else {
+      console.log("Paper beats rock. You lose!");
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "paper") {
+      console.log("Paper and paper. That's a tie!");
+    } else if (computerSelection === "rock") {
+      console.log("Paper beats rock. You win!");
+    } else {
+      console.log("Scissors beat paper. You lose!");
+    }
+  } else {
+    if (computerSelection === "scissors") {
+      console.log("Scissors and scissors. That's a tie!");
+    } else if (computerSelection === "paper") {
+      console.log("Scissors beats paper. You win!");
+    } else {
+      console.log("Rcok beats scissors. You lose!");
+    }
+  }
+}
+
+function playGame() {
+  rPSRound();
+}
