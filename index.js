82161dda165d7c54878edeb205830cb0e8aa9ee1
Ivano <ivanomonego@yahoo.it>
2024-04-16 22:58:54 +0200

    Fixed everything so it works with btns

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 let playerScore = 0;
 let computerScore = 0;
+let roundsCount = 0;
 const btn = document.querySelectorAll("button");
+const results1 = document.createElement("p");
+document.body.appendChild(results1);
+const results = document.createElement("div");
+document.body.appendChild(results);
 
 btn.forEach((item) => {
   item.addEventListener("click", (e) => {
-    console.log(e.target.textContent.toLowerCase());
+    rPSRound(e.target.textContent.toLowerCase(), getComputerChoice());
+    roundsCount += 1;
+    console.log(roundsCount);
+    showScore();
+    playGame();
   });
 });
 
@@ -26,58 +35,54 @@ function getComputerChoice() {
   return computerSelection;
 }
 
-function getPlayerChoice() {}
-
 function rPSRound(playerSelection, computerSelection) {
-  console.log(
-    `You chose ${playerSelection}, while computer chose ${computerSelection}.`
-  );
+  results1.textContent = `You chose ${playerSelection}, while computer chose ${computerSelection}.`;
   if (playerSelection === "rock") {
     if (computerSelection === "rock") {
-      console.log("Rock and rock. That's a tie!");
+      results.textContent = "Rock and rock. That's a tie!";
     } else if (computerSelection === "scissors") {
-      console.log("Rock beats scissors. You win!");
+      results.textContent = "Rock beats scissors. You win!";
       playerScore += 1;
     } else {
-      console.log("Paper beats rock. You lose!");
+      results.textContent = "Paper beats rock. You lose!";
       computerScore += 1;
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "paper") {
-      console.log("Paper and paper. That's a tie!");
+      results.textContent = "Paper and paper. That's a tie!";
     } else if (computerSelection === "rock") {
-      console.log("Paper beats rock. You win!");
+      results.textContent = "Paper beats rock. You win!";
       playerScore += 1;
     } else {
-      console.log("Scissors beat paper. You lose!");
+      results.textContent = "Scissors beat paper. You lose!";
       computerScore += 1;
     }
   } else {
     if (computerSelection === "scissors") {
-      console.log("Scissors and scissors. That's a tie!");
+      results.textContent = "Scissors and scissors. That's a tie!";
     } else if (computerSelection === "paper") {
-      console.log("Scissors beats paper. You win!");
+      results.textContent = "Scissors beats paper. You win!";
       playerScore += 1;
     } else {
-      console.log("Rock beats scissors. You lose!");
+      results.textContent = "Rock beats scissors. You lose!";
       computerScore += 1;
     }
   }
 }
 
-function playGame() {}
+function playGame() {
+  if (roundsCount >= 5) {
+    if (playerScore > computerScore) {
+      console.log("Player wins.");
+    } else if (playerScore < computerScore) {
+      console.log("Computer wins.");
+    } else {
+      console.log("It's a tie.");
+    }
+  }
+}
 
 function showScore() {
   console.log(`Player score is: ${playerScore}.
 Computer score is: ${computerScore}.`);
-}
-
-playGame();
-
-if (playerScore > computerScore) {
-  console.log("Player wins.");
-} else if (playerScore < computerScore) {
-  console.log("Computer wins.");
-} else {
-  console.log("It's a tie.");
 }
